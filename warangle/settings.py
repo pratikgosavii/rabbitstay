b50807fac4583b8e9465f96757f173781f2c1615
pratik gosavi <58395315+pratikgosavii@users.noreply.github.com>
2025-09-02 17:52:32 +0530

    ddfdf

--- warangle/settings.py
+++ warangle/settings.py
@@ -181,7 +181,7 @@ EMAIL_HOST = 'smtp.gmail.com'  # or your email provider
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'Rabbitstay221@gmail.com'
-EMAIL_HOST_PASSWORD = 'alfvyjjihsmhlzfw'  # use App Password if using Gmail
+EMAIL_HOST_PASSWORD = 'qxacifguvstguift'  # use App Password if using Gmail
 
 
 
