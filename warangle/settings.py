13635fc40aa62e8a8a0910c466640057788215bb
pratik gosavi <58395315+pratikgosavii@users.noreply.github.com>
2025-10-05 16:56:31 +0530

    sdsd

--- warangle/settings.py
+++ warangle/settings.py
@@ -178,12 +178,18 @@ WSGI_APPLICATION = 'warangle.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'rabbitstay',
+        'USER': 'rabbituser',
+        'PASSWORD': 'StrongPasswordHere',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 
